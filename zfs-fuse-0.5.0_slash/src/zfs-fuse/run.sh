6b3ee8b75bdfe24442f4ff7ba5c1d665128284ad
yanovich <yanovich@3eda493b-6a19-0410-b2e0-ec8ea4dd8fda>
2009-11-09 23:26:09 +0000

    fix core dump setting
    git-svn-id: svn+ssh://frodo/cluster/svn/projects/zfs@8946 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- zfs-fuse-0.5.0_slash/src/zfs-fuse/run.sh
+++ zfs-fuse-0.5.0_slash/src/zfs-fuse/run.sh
@@ -1,8 +1,23 @@
 #!/bin/sh
 # $Id$
 
+usage()
+{
+	echo "usage: $0 [-c coresiz]" >&2
+	exit 1
+}
+
+core=unlimited
+
+while getopts "c:" c; do
+	case $c in
+	c) core=$OPTARG;;
+	*) usage;;
+	esac
+done
+
 dir=$(dirname $0)
 
-ulimit -c unlimited
+ulimit -c $core
 
 $dir/zfs-fuse --no-daemon
