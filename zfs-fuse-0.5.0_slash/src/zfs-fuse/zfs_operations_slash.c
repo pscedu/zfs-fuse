93fc0dbc40a75809a6120f5f1c08c75c3be1f4e0
zhihui <zhihui@3eda493b-6a19-0410-b2e0-ec8ea4dd8fda>
2010-02-17 20:00:52 +0000

    Add fidlink code to opencreate() path
    git-svn-id: svn+ssh://frodo/cluster/svn/projects/zfs@10392 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- zfs-fuse-0.5.0_slash/src/zfs-fuse/zfs_operations_slash.c
+++ zfs-fuse-0.5.0_slash/src/zfs-fuse/zfs_operations_slash.c
@@ -685,6 +685,15 @@ zfsslash2_opencreate(void *vfsdata, uint64_t ino,
 
 	znode_t *znode;
 
+#ifdef NAMESPACE_EXPERIMENTAL
+	error = zfsslash2_fidlink(zfsvfs, &dvp, fg->fg_fid, FIDLINK_LOOKUP);
+	if(error) {
+		ZFS_EXIT(zfsvfs);
+		/* If the inode we are trying to get was recently deleted
+		   dnode_hold_impl will return EEXIST instead of ENOENT */
+		return error == EEXIST ? ENOENT : error;
+	}
+#else
 	error = zfs_zget(zfsvfs, real_ino, &znode, B_FALSE);
 	if (error) {
 		ZFS_EXIT(zfsvfs);
@@ -696,6 +705,7 @@ zfsslash2_opencreate(void *vfsdata, uint64_t ino,
 	ASSERT(znode != NULL);
 	vp = ZTOV(znode);
 	ASSERT(vp != NULL);
+#endif
 
 	if (flags & FCREAT) {
 		enum vcexcl excl;
