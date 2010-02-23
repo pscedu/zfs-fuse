41d3e615ca464e2f2ba25d06bc611b33d5be5904
yanovich <yanovich@3eda493b-6a19-0410-b2e0-ec8ea4dd8fda>
2010-02-23 03:08:33 +0000

    sync protos
    git-svn-id: svn+ssh://frodo/cluster/svn/projects/zfs@10449 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- zfs-fuse-0.5.0_slash/src/zfs-fuse/zfs_slashlib.h
+++ zfs-fuse-0.5.0_slash/src/zfs-fuse/zfs_slashlib.h
@@ -26,26 +26,26 @@ typedef struct file_info {
 } file_info_t;
 #endif
 
-int	zfsslash2_access(void *, uint64_t, int, const struct slash_creds *);
-int	zfsslash2_fsync(void *, uint64_t, const struct slash_creds *, int, void *);
-int	zfsslash2_getattr(void *, uint64_t, const struct slash_creds *, struct srt_stat *, uint64_t *);
-int	zfsslash2_link(void *, uint64_t, uint64_t, const char *, struct slash_fidgen *, const struct slash_creds *, struct srt_stat *);
-int	zfsslash2_lookup(void *, uint64_t, const char *, struct slash_fidgen *, const struct slash_creds *, struct srt_stat *, int);
-int	zfsslash2_mkdir(void *, uint64_t, const char *, mode_t, const struct slash_creds *, struct srt_stat *, struct slash_fidgen *, int);
-int	zfsslash2_mknod(void *, uint64_t, const char *, mode_t, dev_t);
-int	zfsslash2_opencreate(void *, uint64_t, const struct slash_creds *, int, mode_t, const char *, struct slash_fidgen *, struct srt_stat *, void **);
-int	zfsslash2_opendir(void *, uint64_t, const struct slash_creds *, struct slash_fidgen *, struct srt_stat *, void **, int);
-int	zfsslash2_read(void *, uint64_t, const struct slash_creds *, void *, size_t, size_t *, off_t, void *);
-int	zfsslash2_readdir(void *, uint64_t, const struct slash_creds *, size_t, off_t, void *, size_t *, void  *, int, void *);
-int	zfsslash2_readlink(void *, uint64_t, char *, const struct slash_creds *);
-int	zfsslash2_release(void *, uint64_t, const struct slash_creds *, void *);
-int	zfsslash2_rename(void *, uint64_t, const char *, uint64_t, const char *, const struct slash_creds *);
-int	zfsslash2_rmdir(void *, uint64_t, const char *, const struct slash_creds *);
-int	zfsslash2_setattr(void *, uint64_t, const struct srt_stat *, int, const struct slash_creds *, struct srt_stat *, void *);
-int	zfsslash2_statfs(void *, struct statvfs *, uint64_t);
-int	zfsslash2_symlink(void *, const char *, uint64_t, const char *, const struct slash_creds *, struct srt_stat *, struct slash_fidgen *);
-int	zfsslash2_unlink(void *, uint64_t, const char *, const struct slash_creds *);
-int	zfsslash2_write(void *, uint64_t, const struct slash_creds *, const void *, size_t, size_t *, off_t, void *);
+int	zfsslash2_access(uint64_t, int, const struct slash_creds *);
+int	zfsslash2_fsync(const struct slash_creds *, int, void *);
+int	zfsslash2_getattr(uint64_t, const struct slash_creds *, struct srt_stat *, uint64_t *);
+int	zfsslash2_link(uint64_t, uint64_t, const char *, struct slash_fidgen *, const struct slash_creds *, struct srt_stat *);
+int	zfsslash2_lookup(uint64_t, const char *, struct slash_fidgen *, const struct slash_creds *, struct srt_stat *, int);
+int	zfsslash2_mkdir(uint64_t, const char *, mode_t, const struct slash_creds *, struct srt_stat *, struct slash_fidgen *, int);
+int	zfsslash2_mknod(uint64_t, const char *, mode_t, dev_t);
+int	zfsslash2_opencreate(uint64_t, const struct slash_creds *, int, mode_t, const char *, struct slash_fidgen *, struct srt_stat *, void **);
+int	zfsslash2_opendir(uint64_t, const struct slash_creds *, struct slash_fidgen *, struct srt_stat *, void **, int);
+int	zfsslash2_read(const struct slash_creds *, void *, size_t, size_t *, off_t, void *);
+int	zfsslash2_readdir(const struct slash_creds *, size_t, off_t, void *, size_t *, void  *, int, void *);
+int	zfsslash2_readlink(uint64_t, char *, const struct slash_creds *);
+int	zfsslash2_release(const struct slash_creds *, void *);
+int	zfsslash2_rename(uint64_t, const char *, uint64_t, const char *, const struct slash_creds *);
+int	zfsslash2_rmdir(uint64_t, const char *, const struct slash_creds *);
+int	zfsslash2_setattr(uint64_t, const struct srt_stat *, int, const struct slash_creds *, struct srt_stat *, void *);
+int	zfsslash2_statfs(struct statvfs *, uint64_t);
+int	zfsslash2_symlink(const char *, uint64_t, const char *, const struct slash_creds *, struct srt_stat *, struct slash_fidgen *);
+int	zfsslash2_unlink(uint64_t, const char *, const struct slash_creds *);
+int	zfsslash2_write(const struct slash_creds *, const void *, size_t, size_t *, off_t, void *);
 
 int	do_init(void);
 void	do_exit(void);
@@ -55,4 +55,4 @@ void	do_exit(void);
 
 extern void *zfsVfs;
 
-#endif
+#endif /* _ZFS_SLASHLIB_H_ */
