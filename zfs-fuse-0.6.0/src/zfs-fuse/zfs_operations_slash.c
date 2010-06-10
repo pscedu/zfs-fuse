336ec87269bda4d05248e84e163246855bc5965a
zhihui <zhihui@3eda493b-6a19-0410-b2e0-ec8ea4dd8fda>
2010-06-10 15:39:52 +0000

    The fidc cache constructor does not need to do anything except for dir and files.  At least opencreate() on symlink will cause ENOSYS (35).
    git-svn-id: svn+ssh://frodo/cluster/svn/projects/zfs@12000 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- zfs-fuse-0.6.0/src/zfs-fuse/zfs_operations_slash.c
+++ zfs-fuse-0.6.0/src/zfs-fuse/zfs_operations_slash.c
@@ -843,7 +843,7 @@ zfsslash2_opencreate(mdsio_fid_t ino, const struct slash_creds *slcrp,
 	 * if the vnode is of type VLNK.  This is because its 
 	 * v_op is set to fs_nosys() for VOP_OPEN(). 
 	 */
-	error = VOP_OPEN(&vp, flags, cred, NULL);
+	error = VOP_OPEN(&vp, flags, cred, NULL);		/* zfs_open() */
 
 	ASSERT(old_vp == vp);
 
