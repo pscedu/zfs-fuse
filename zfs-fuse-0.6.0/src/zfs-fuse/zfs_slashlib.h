eac6461552e1396dcfb1b6c3b2d68662f1e47cb0
yanovich <yanovich@3eda493b-6a19-0410-b2e0-ec8ea4dd8fda>
2010-07-16 17:53:16 +0000

    sync proto for opencreate
    git-svn-id: svn+ssh://frodo/cluster/svn/projects/zfs@12405 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- zfs-fuse-0.6.0/src/zfs-fuse/zfs_slashlib.h
+++ zfs-fuse-0.6.0/src/zfs-fuse/zfs_slashlib.h
@@ -26,11 +26,9 @@ int	zfsslash2_getattr(mdsio_fid_t, const struct slash_creds *, struct srt_stat *
 int	zfsslash2_link(mdsio_fid_t, mdsio_fid_t, const char *, struct slash_fidgen *, const struct slash_creds *, struct srt_stat *, sl_log_update_t);
 int	zfsslash2_lookup(mdsio_fid_t, const char *, struct slash_fidgen *, mdsio_fid_t *, const struct slash_creds *, struct srt_stat *);
 int	zfsslash2_lookup_slfid(slfid_t, const struct slash_creds *, struct srt_stat *, mdsio_fid_t *);
-int	zfsslash2_mkdir(mdsio_fid_t, const char *, mode_t, const struct slash_creds *, struct srt_stat *, \
-	struct slash_fidgen *, mdsio_fid_t *, sl_log_update_t, sl_getslfid_cb_t);
+int	zfsslash2_mkdir(mdsio_fid_t, const char *, mode_t, const struct slash_creds *, struct srt_stat *, struct slash_fidgen *, mdsio_fid_t *, sl_log_update_t, sl_getslfid_cb_t);
 int	zfsslash2_mknod(mdsio_fid_t, const char *, mode_t, dev_t);
-int	zfsslash2_opencreate(mdsio_fid_t, const struct slash_creds *, int, mode_t, const char *, \
-	struct slash_fidgen *, mdsio_fid_t *, struct srt_stat *, void **, sl_log_update_t, sl_getslfid_cb_t);
+int	zfsslash2_opencreate(mdsio_fid_t, const struct slash_creds *, int, int, mode_t, const char *, struct slash_fidgen *, mdsio_fid_t *, struct srt_stat *, void **, sl_log_update_t, sl_getslfid_cb_t);
 int	zfsslash2_opendir(mdsio_fid_t, const struct slash_creds *, struct slash_fidgen *, void **);
 int	zfsslash2_read(const struct slash_creds *, void *, size_t, size_t *, off_t, void *);
 int	zfsslash2_readdir(const struct slash_creds *, size_t, off_t, void *, size_t *, size_t *, void  *, int, void *);
@@ -40,8 +38,7 @@ int	zfsslash2_rename(mdsio_fid_t, const char *, mdsio_fid_t, const char *, const
 int	zfsslash2_rmdir(mdsio_fid_t, const char *, const struct slash_creds *, sl_log_update_t);
 int	zfsslash2_setattr(mdsio_fid_t, const struct srt_stat *, int, const struct slash_creds *, struct srt_stat *, void *, sl_log_update_t);
 int	zfsslash2_statfs(struct statvfs *);
-int	zfsslash2_symlink(const char *, mdsio_fid_t, const char *, const struct slash_creds *, struct srt_stat *, \
-	struct slash_fidgen *, mdsio_fid_t *, sl_getslfid_cb_t, sl_log_update_t);
+int	zfsslash2_symlink(const char *, mdsio_fid_t, const char *, const struct slash_creds *, struct srt_stat *, struct slash_fidgen *, mdsio_fid_t *, sl_getslfid_cb_t, sl_log_update_t);
 int	zfsslash2_unlink(mdsio_fid_t, const char *, const struct slash_creds *, sl_log_update_t);
 int	zfsslash2_write(const struct slash_creds *, const void *, size_t, size_t *, off_t, void *, sl_log_write_t, void *);
 
@@ -54,16 +51,13 @@ void	do_exit(void);
 #define zfs_exit()	do_exit()
 
 int	zfsslash2_replay_create(slfid_t, slfid_t, struct srt_stat *, char *);
-int	zfsslash2_replay_mkdir(slfid_t, slfid_t, struct srt_stat *, char *);
 int	zfsslash2_replay_link(slfid_t, slfid_t, char *);
-int	zfsslash2_replay_symlink(slfid_t, slfid_t, struct srt_stat *, char *, char *);
-
-int	zfsslash2_replay_rmdir(slfid_t, slfid_t, char *);
-int	zfsslash2_replay_unlink(slfid_t, slfid_t, char *);
-
-int	zfsslash2_replay_setattr(slfid_t, struct srt_stat *, uint);
-
+int	zfsslash2_replay_mkdir(slfid_t, slfid_t, struct srt_stat *, char *);
 int	zfsslash2_replay_rename(slfid_t, const char *, slfid_t, const char *);
+int	zfsslash2_replay_rmdir(slfid_t, slfid_t, char *);
+int	zfsslash2_replay_setattr(slfid_t, struct srt_stat *, uint);
+int	zfsslash2_replay_symlink(slfid_t, slfid_t, struct srt_stat *, char *, char *);
+int	zfsslash2_replay_unlink(slfid_t, slfid_t, char *);
 
 uint64_t	zfsslash2_last_synced_txg(void);
 uint64_t	zfsslash2_return_synced(void);
