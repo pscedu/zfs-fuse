745cff71e439a3e6d1915cc5a99c809dd66d5132
zhihui <zhihui@3eda493b-6a19-0410-b2e0-ec8ea4dd8fda>
2011-03-07 20:14:41 +0000

    debug code
    git-svn-id: svn+ssh://frodo/cluster/svn/projects/zfs@15719 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- src/zfs-fuse/zfs_operations_slash.c
+++ src/zfs-fuse/zfs_operations_slash.c
@@ -1167,7 +1167,7 @@ zfsslash2_opencreate(mdsio_fid_t ino, const struct slash_creds *slcrp,
 			error = zfsslash2_fidlink(
 			    VTOZ(vp)->z_phys->zp_s2fid,
 			    FIDLINK_CREATE, vp, NULL);
-#if 0
+#if 1
 			fprintf(stderr, "create: name = %s, fid = 0x%lx, errno = %d\n", 
 				name, vattr.va_fid, errno);
 #endif
@@ -1382,6 +1382,11 @@ zfsslash2_mkdir(mdsio_fid_t parent, const char *name, mode_t mode,
 
 	if (sstb || mfp)
 		error = fill_sstb(vp, mfp, sstb, &cred);
+
+#if 0
+	fprintf(stderr, "mkdir: name = %s, fid = 0x%lx, errno = %d\n", 
+				name, vattr.va_fid, errno);
+#endif
 
  out:
 	if (vp)
