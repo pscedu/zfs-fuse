1766c711770f7a0db54cb0cc70bc87c671eed8ff
zhihui-slash2 <zhihui@psc.edu>
2016-03-18 15:08:24 -0400

    comment

--- src/lib/libzpool/zio.c
+++ src/lib/libzpool/zio.c
@@ -244,6 +244,8 @@ zio_buf_alloc(size_t size)
 	/* 
 	 * 03/18/2016: hit no memory when size is 4096 with bits without
  	 * my arc_meta_limit experimental patch in arc_evict_needed().
+ 	 *
+ 	 * Possible solution: arc_kmem_reap_now().
  	 */
 	return (kmem_cache_alloc(zio_buf_cache[c], KM_PUSHPAGE));
 }
