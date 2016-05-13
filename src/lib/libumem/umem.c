55e25a3e59e17fdb7d0b0d5f317487018cf47af5
zhihui-slash2 <zhihui@psc.edu>
2016-05-13 10:15:42 -0400

    fix obsolete comment

--- src/lib/libumem/umem.c
+++ src/lib/libumem/umem.c
@@ -3217,7 +3217,9 @@ int should_reap_umem_default(void)
 	 * Hit ENOMEM today. umem_default_arena is once again exhausted, while
 	 * arc_stats.arcstat_size.value.ui64 is only 12843403664.
 	 *
-	 * (gdb) p nb_mmap
+	 * (gdb) p nb_mmap_curr
+	 * (gdb) p nb_mmap_ceil
+	 * (gdb) p nb_mmap_raise
 	 * (gdb) p umem_default_arena->vm_kstat.vk_mem_inuse
 	 * (gdb) p umem_default_arena->vm_kstat.vk_mem_total
 	 *
