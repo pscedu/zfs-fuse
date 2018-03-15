9765024ea40581ff4aaba90c69981229695878b9
zhihui-slash2 <zhihui@psc.edu>
2018-03-15 16:04:42 -0400

    copyright

--- src/zfs-fuse/zfs_slashlib.h
+++ src/zfs-fuse/zfs_slashlib.h
@@ -1,5 +1,23 @@
 /* $Id$ */
-/* %GPL_LICENSE% */
+/*
+ * %GPL_START_LICENSE%
+ * ---------------------------------------------------------------------
+ * Copyright 2014-2016, Pittsburgh Supercomputing Center
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ * PURPOSE.  See the GNU General Public License contained in the file
+ * `COPYING-GPL' at the top of this distribution or at
+ * https://www.gnu.org/licenses/gpl-2.0.html for more details.
+ * ---------------------------------------------------------------------
+ * %END_LICENSE%
+ */
 
 #ifndef _ZFS_SLASHLIB_H_
 #define _ZFS_SLASHLIB_H_
